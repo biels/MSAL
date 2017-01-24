2b3c34a9114ea2c4d071b4043d124ca02c4036a2
Biel Simon Rojas <bielsimon2@gmail.com>
2017-01-24 11:04:13 +0100

    Configured official Spigot and BungeeCord repositories

---
+++ src/main/java/com/biel/msal/MSALPlugin.java
@@ -0,0 +1,16 @@
+package com.biel.msal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by biel on 22/01/17.
+ */
+public class MSALPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        //Start MSAL
+
+    }
+}
